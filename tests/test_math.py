36e539ba8f47f1999d54d42e0d783c8118545fbf
doaa-altarawy <doaa.altarawy@gmail.com>
2017-07-25 15:07:34 -0400

    Add mult test

--- tests/test_math.py
+++ tests/test_math.py
@@ -8,3 +8,7 @@ import pytest
 def test_add():
     assert fc.math.add(5, 3) == 8
     assert fc.math.add(-3, 5) == 2
+
+def test_mult():
+    assert fc.mult(5, 3) == 15
+    assert fc.mult(-3, 5) == -15
